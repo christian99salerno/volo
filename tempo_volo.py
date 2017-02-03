1c5aa8dd1db3c0c2a692bf276242235bceb711ca
christian99salerno <christian.salerno05@gmail.com>
2017-02-03 17:40:46 +0100

    passo dopo passo si spicca il volo, ma non lo so fare

---
+++ tempo_volo.py
@@ -0,0 +1,23 @@
+
+print
+
+print "Tempo di Volo"
+print "Determina la durata massima di volo conoscendo"
+print "la quantita' di carburante ed il consumo orario"
+
+print
+
+carb=input("Carburante (in galloni): ")
+cons=input("Consumo Orario (in galloni/h): ")
+
+print 
+
+if(carb<0 or cons<0): 
+    print"ERRORE!!!"
+    print
+
+else:
+    #che cosa devo scrivere ????? :( :( io non lo so ;(
+
+
+   
